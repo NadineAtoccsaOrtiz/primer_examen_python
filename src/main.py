162d77f52889678f6d1b9c838aa4350754805ec2
Nadine <nadineatoccsaortiz@gmail.com>
2023-10-11 12:06:54 -0500

    examen

--- src/main.py
+++ src/main.py
@@ -1,18 +1,32 @@
 #1.la siguiente funcion debera retornar la suma de sus parametros
 def suma(a,b):
   #reemplazar pass por la sintaxis correcta
-  pass
+  suma=a+b
+  return suma
 
 #2. la funcion debera retornar 'es menor' o 'es mayor' segun la edad que pase por parametro
 def is_greater_than(edad):
+  if edad>=18:
   #reemplazar pass por la sintaxis correcta
-  pass
+    return 'Eres mayor de edad'
+  else:
+    return 'eres menor de edad'
 
-#3. la funcion recibe como parametros dos datos el primero arr recibe una array(lista) el segundo num un numero entero positivo, la funcion debera retornar un nuevo array con el num insertado en la tercera posicion del array
+
+
+#3. la funcion recibe como parametros dos datos el primero arr recibe una array(lista) el segundo num un numero entero positivo,
+#  la funcion debera retornar un nuevo array con el num insertado en la tercera posicion del array
 def new_array(arr,num):
+    nuevo=new_array(arr,num)
+return nuevo
+
   #reemplazar pass por la sintaxis correcta
-  pass
+  
+
+
 
 #4. la funcion recibe una array debera retornar la suma de los numero ejm: [4,2,8,10] retornara 24
 def suma_array(arr):
-  pass
+  arr=[]
+  suma+=arr
+  return suma_array
